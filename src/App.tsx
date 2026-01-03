dffe2414a2eafb8e46aed5566faf00a323880106
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2026-01-03 14:54:41 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import './globals.css';
 import SignInForm from './_auth/forms/SignInForm';
 import SignUpForm from './_auth/forms/SignUpForm';
 import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* Public Routes */}
-        <Route path="/sign-in" element={<SignInForm/>} />
-        <Route path="/sign-up" element={<SignUpForm/>} />
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
 
         {/* Private Routes */}
-        <Route index element={<Home/> } />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
