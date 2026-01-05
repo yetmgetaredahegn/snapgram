a92966acbd1624bf8717ce84ae84d99d57f467e9
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2026-01-05 23:20:18 +0300

    fix: layering issue

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -16,14 +16,14 @@ import { z } from "zod"
 
 
 function onSubmit(values: z.infer<typeof SignUpValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-  }
+  // Do something with the form values.
+  // ✅ This will be type-safe and validated.
+  console.log(values)
+}
 
 
 const SignUpForm = () => {
-    const form = useForm<z.infer<typeof SignUpValidation>>({
+  const form = useForm<z.infer<typeof SignUpValidation>>({
     resolver: zodResolver(SignUpValidation),
     defaultValues: {
       username: "",
@@ -34,28 +34,45 @@ const SignUpForm = () => {
   })
   return (
     <Form {...form}>
-      <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo" />
+      <div className="w-full max-w-[420px] mx-auto flex flex-col items-center space-y-6">
+        
+        {/* Logo */}
+        <div className="mb-4">
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            className="h-10"
+          />
+          <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create new account</h2>
+        </div>
+
+        {/* Form */}
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-8"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="w-full">
+            Submit
+          </Button>
+        </form>
       </div>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
     </Form>
   )
 }
